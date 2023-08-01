0833c68e10105e972f16f0e8ae1e5a0e86bf5896
Soufiane Mourragui <soufiane.mourragui@gmail.com>
2023-08-01 09:38:53 +0200

    pre-commit updates compliance

--- sobolev_alignment/sobolev_alignment.py
+++ sobolev_alignment/sobolev_alignment.py
@@ -547,9 +547,11 @@ class SobolevAlignment:
         del _generated_data
         gc.collect()
 
-        if save_mmap is not None and type(save_mmap) == str:
-            np.save(open(f"{save_mmap}/{data_source}_artificial_input.npy", "wb"), artificial_samples)
-            artificial_samples = np.load(f"{save_mmap}/{data_source}_artificial_input.npy", mmap_mode="r")
+        if save_mmap is not None and type(save_mmap) is str:
+            np.save(open(os.path.join(save_mmap, "%s_artificial_input.npy" % (data_source)), "wb"), artificial_samples)
+            artificial_samples = np.load(
+                os.path.join(save_mmap, "%s_artificial_input.npy" % (data_source)), mmap_mode="r"
+            )
             gc.collect()
 
         return artificial_samples, artificial_batches_, artificial_covariates_
@@ -705,7 +707,7 @@ class SobolevAlignment:
         frob_norm_source: bool = False,
     ):
         # Save embedding
-        if save_mmap is not None and type(save_mmap) == str:
+        if save_mmap is not None and type(save_mmap) is str:
             self._save_mmap = save_mmap
             self._memmap_embedding(
                 data_source=data_source, artificial_embeddings=artificial_embeddings, save_mmap=save_mmap
@@ -722,7 +724,7 @@ class SobolevAlignment:
             # Frobenius norm scaling
             artificial_samples = self._frobenius_normalisation(data_source, artificial_samples, frob_norm_source)
 
-            if save_mmap is not None and type(save_mmap) == str:
+            if save_mmap is not None and type(save_mmap) is str:
                 # Re-save
                 np.save(open(f"{save_mmap}/{data_source}_artificial_input.npy", "wb"), artificial_samples)
                 artificial_samples = np.load(f"{save_mmap}/{data_source}_artificial_input.npy", mmap_mode="r")
